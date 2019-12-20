7b14400113ab2beb2fe45bc2e5ed2560d16c863f
Artak Mikayelyan <miqart@mail.ru>
2019-12-20 00:55:50 -0800

    Updates

---
+++ precache-manifest.8cd99109dc1446888765ecd37085e955.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bea70bfc8294a2bf78e439a05d6702",
+    "url": "/react-blogpost/index.html"
+  },
+  {
+    "revision": "2d8e893730579f7e8f8e",
+    "url": "/react-blogpost/static/css/2.4e153dcf.chunk.css"
+  },
+  {
+    "revision": "aa78ed363914bef3d23c",
+    "url": "/react-blogpost/static/css/main.88799620.chunk.css"
+  },
+  {
+    "revision": "2d8e893730579f7e8f8e",
+    "url": "/react-blogpost/static/js/2.bb97b996.chunk.js"
+  },
+  {
+    "revision": "aa78ed363914bef3d23c",
+    "url": "/react-blogpost/static/js/main.c6b9a4d2.chunk.js"
+  },
+  {
+    "revision": "5f26fdeb86d1cdb9dd8e",
+    "url": "/react-blogpost/static/js/runtime-main.2692c840.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-blogpost/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-blogpost/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-blogpost/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-blogpost/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-blogpost/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
